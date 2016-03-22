7b42c57185c0779a12120a597ddaf0ef8fbbedae
orpnoch <have.no.named@gmail.com>
2016-03-22 21:16:31 +0700

    customizing spinner style

--- app/src/main/java/xanctuary/transgames_client/Voucher_1.java
+++ app/src/main/java/xanctuary/transgames_client/Voucher_1.java
@@ -9,5 +9,6 @@ public class Voucher_1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voucher_1);
+
     }
 }
